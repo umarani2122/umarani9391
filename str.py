04240de1fbd2a09679054e8286330cdf8601b928
umarani2122 <umaranikanchani2122@gmail.com>
2025-06-15 23:40:24 -0700

    Create str.py

---
+++ str.py
@@ -0,0 +1,34 @@
+# str_1="keerthika"
+# print( str_1)   #keerthika  (double quotes uses)
+# #########################################################################################3
+# str_2= ' sravanthi'
+# print( str_2) #sravanthi      #(single quotes)
+
+# #####################################################################################
+# str_3 = 'umarani'
+# print( str_3.lower()) #umarani     #(lower cse uses)
+ 
+
+
+#################################################################################3
+
+# str_4='UMARANI'
+# print( str_4.lower())  #umarani #( lower case)
+########################################################################
+
+# str_5=""" munnu """
+# print( str_5)    # munnu  ( thrible quotes)
+#########################################################3
+
+# str_6=" threee 'gils' are so funny"
+# print(str_6)    # threee 'gils' are so funny  (using double quotes iside insingle quotes)
+####################################
+# str_7 = " manish"
+# print( str_7.upper() ) # MANISH (USES UPPER CASE )
+#########################################
+str_8="my name is xyz"
+print(str_8.capitalize()) #My name is xyz  # capitalize uses
+
+#################################
+# str_9 = "laPtop"
+# print( str_9.swapcase()) # SWAP CASE
